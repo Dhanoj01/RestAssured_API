41fa0881a48bde5ec07b33e0c18d1df6dfebbf87
dhanojkumarsingh <dhanojkumarsingh@localhost>
2026-02-04 10:54:43 +0530

    updated class ParameterDemo

--- RestAssured/src/test/java/Day2/ParameterDemo.java
+++ RestAssured/src/test/java/Day2/ParameterDemo.java
@@ -1,5 +1,7 @@
 package Day2;
-
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
 import org.testng.annotations.Test;
 
 public class ParameterDemo {
@@ -11,7 +13,22 @@ public class ParameterDemo {
 		given()
 		    .pathParam("country" , "India")
 		.when()
-		    .get("https://restcountries.com/v2/name/{country}")
+		    .get("https://restcountries.com/v2/name/{country}")  //https://restcountries.com/v2/name/India
+		.then()
+		    .statusCode(200)
+		    .log().body();
+	}
+	
+	
+	@Test
+	public void queryParamm()
+	{
+		
+		given()
+		    .queryParam("page","2")
+		    .queryParam("id", "5")
+		.when()
+		    .get("https://reqres.in/api/users")     //https://reqres.in/api/users?page=2&id=2
 		.then()
 		    .statusCode(200)
 		    .log().body();
