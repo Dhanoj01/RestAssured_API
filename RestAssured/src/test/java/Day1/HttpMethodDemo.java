e2cfb174ff8e26e2f6c0eb2c15047b6d0c1a1ec9
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-02-11 00:18:56 +0530

    https methods

--- RestAssured/src/test/java/Day1/HttpMethodDemo.java
+++ RestAssured/src/test/java/Day1/HttpMethodDemo.java
@@ -4,6 +4,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.everyItem;
 import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.equalTo;
 
 import java.util.HashMap;
 
@@ -11,7 +12,9 @@ import org.testng.annotations.Test;
 
 public class HttpMethodDemo {
 
-    @Test(enabled = false)
+	int userId;
+	
+    @Test(enabled = true)
     public void getMethod() {
 
         given()
@@ -33,7 +36,62 @@ public class HttpMethodDemo {
     public void postMethod()
     {
     	
-    	HashMap<String , String> payload = new HashMap<>();
+    	HashMap<String , String> data = new HashMap<>();
     	
+    	data.put("name", "pavan");
+    	data.put("job", "trainer");
+    	
+    userId=	given()
+    	   .contentType("application/json")
+    	   .body(data)
+    	.when()
+    	   .post("https://jsonplaceholder.typicode.com/users") 
+    	.then()
+    	   .statusCode(201)
+    	   .header("Content-Type", containsString("application/json"))
+    	   .body("name", equalTo("pavan"))
+    	   .body("job", equalTo("trainer"))
+    	   .log().all()
+    	   .extract().jsonPath().getInt("id");
+    	
+    	
+    }
+    
+    @Test(priority=3 , enabled = true)
+    public void putMethod()
+    {
+    	
+    	HashMap<String , String> data = new HashMap<>();
+    	
+    	data.put("name", "dhanoj");
+    	data.put("job", "engineer");
+    	
+        	given()
+    	   .contentType("application/json")
+    	   .pathParam("userId", userId)
+    	   .body(data)
+    	.when()
+    	   .put("https://jsonplaceholder.typicode.com/users/{userId}") 
+    	.then()
+    	   .statusCode(200)
+    	   .header("Content-Type", containsString("application/json"))
+    	   .body("name", equalTo("dhanoj"))
+    	   .body("job", equalTo("engineer"))
+    	   .log().all();
+    	   
+    }
+    
+    @Test(priority=4 , enabled = true)
+    public void deleteMethod()
+    {
+    	
+        given() 
+    	   .pathParam("userId", userId) 
+    	.when()
+    	   .delete("https://jsonplaceholder.typicode.com/users/{userId}") 
+    	.then()
+    	   .statusCode(200);
+    	
+    	   
     }
 }
