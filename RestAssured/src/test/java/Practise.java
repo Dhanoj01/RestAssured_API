7b0d9257163a17ceeabbb30a7ae9e27f7f54a7b3
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-09-12 12:35:26 +0530

    commit

--- RestAssured/src/test/java/Practise.java
+++ RestAssured/src/test/java/Practise.java
@@ -1,6 +1,26 @@
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.HashMap;
+
+import org.testng.annotations.Test;
+
+
 public class Practise{
 	
-	
-	
+	@Test
+     public void getRequest()
+     {
+    	 
+		given()
+		.when()
+		   .get("https://www.google.com/")
+		.then() 
+		 .statusCode(200);
+     }
 	
 }
